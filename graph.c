c73dfff0262228786b0ea30269d8b6e79c50c13c
Timothée <66522009+timotheetacca@users.noreply.github.com>
2026-02-22 07:23:54 +0100

    Add predecessor_matrix, separate display matrix from display graph

--- graph.c
+++ graph.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "graph.h"
 
 graph* load_graph(char *filename) {
@@ -13,12 +11,22 @@ graph* load_graph(char *filename) {
     fscanf(graph_text, "%d", &g->num_arcs);
 
     g->matrix = malloc(g->num_vertices * sizeof(int*));
+    g->predecessor_matrix = malloc(g->num_vertices * sizeof(int*));
 
     // Initialisation of empty matrix
     for (int i = 0; i < g->num_vertices; i++) {
         g->matrix[i] = malloc(g->num_vertices * sizeof(int));
+        g->predecessor_matrix[i] = malloc(g->num_vertices * sizeof(int));
+
         for (int j = 0; j < g->num_vertices; j++) {
-            g->matrix[i][j] = 999999;
+            if (i == j) {
+                g->matrix[i][j] = 0;
+                g->predecessor_matrix[i][j] = 999999;
+            }
+            else {
+                g->matrix[i][j] = 999999;
+                g->predecessor_matrix[i][j] = 999999;
+            }
         }
     }
 
@@ -27,6 +35,7 @@ graph* load_graph(char *filename) {
         int source, destination, weight;
         fscanf(graph_text, "%d %d %d", &source, &destination, &weight);
         g->matrix[source][destination] = weight;
+        g->predecessor_matrix[source][destination] = source;
 
         // Counts the length of the int (for display)
         int len = 0;
@@ -48,32 +57,41 @@ graph* load_graph(char *filename) {
     return g;
 }
 
+void display_matrix(int **matrix, int longest_weight, int num_vertices,char *replace_char) {
 
-void display_graph(graph *g) {
-    printf("Vertices: %d\n", g->num_vertices);
-    printf("Arcs: %d\n", g->num_arcs);
-    int box_width = g->longest_weight + 2;
+    int box_width = longest_weight + 2;
 
     // Print column headers
     printf("%*s", box_width, "");
-    for (int j = 0; j < g->num_vertices; j++) {
-        printf("%*d", box_width, j);
+    for (int i = 0; i < num_vertices; i++) {
+        printf("%*d", box_width, i);
     }
     printf("\n");
 
-    for (int i = 0; i < g->num_vertices; i++) {
+    for (int i = 0; i < num_vertices; i++) {
         // Print row header
         printf("%*d", box_width, i);
 
-        for (int j = 0; j < g->num_vertices; j++) {
-            if (g->matrix[i][j] == 999999) {
-                printf("%*s∞", box_width-1, "");
+        for (int j = 0; j < num_vertices; j++) {
+            if (matrix[i][j] == 999999) {
+                printf("%*s", box_width, replace_char);
             }
             else {
-                printf("%*d", box_width, g->matrix[i][j]);
+                printf("%*d", box_width, matrix[i][j]);
             }
         }
         printf("\n");
     }
-    return;
+}
+
+void display_graph(graph *g) {
+    printf("Vertices: %d\n", g->num_vertices);
+    printf("Arcs: %d\n", g->num_arcs);
+    printf("Adjancy matrix:\n");
+
+    display_matrix(g->matrix, g->longest_weight, g->num_vertices, "    ∞");
+
+    printf("\n\nPredecessor matrix:\n");
+
+    display_matrix(g->predecessor_matrix, g->longest_weight, g->num_vertices, "_");
 }
--- graph.h
+++ graph.h
@@ -1,7 +1,3 @@
-//
-// Created by Timothée on 18/02/2026.
-//
-
 #ifndef FLOYD_WARSHALL_ALGORITHM_GRAPH_H
 #define FLOYD_WARSHALL_ALGORITHM_GRAPH_H
 
@@ -11,10 +7,13 @@ typedef struct {
     int num_vertices;
     int num_arcs;
     int **matrix;
+    int **predecessor_matrix;
     int longest_weight;
+
 } graph;
 
 graph* load_graph(char *filename);
+void display_matrix(int **matrix, int longest_weight, int num_vertices,char *replace_char);
 void display_graph(graph *g);
 
 #endif //FLOYD_WARSHALL_ALGORITHM_GRAPH_H
